6b98383490e1e963fa73b3338e5b92ae43a0d18c
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2020-12-08 17:58:31 -0800

    Add project files.

---
+++ Cards/Cards/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Cards
+{
+    public class Card
+    {
+        Texture2D cardTexture;
+        Rectangle cardRectangle;
+
+        public Card(ContentManager c, String contentName, Rectangle rectangle)
+        {
+            cardTexture = c.Load<Texture2D>(contentName);
+            cardRectangle = rectangle;
+        }
+
+        public void Draw(GraphicsDevice graphicsDevice)
+        {
+            SpriteBatch sb = new SpriteBatch(graphicsDevice);
+            sb.Begin();
+            sb.Draw(cardTexture, cardRectangle, Color.White);
+            sb.End();
+        }
+    }
+}
---
+++ Cards/Cards/Game1.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Cards
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Card tenOfSpades;
+        Card aceOfDiamonds;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            tenOfSpades = new Card(this.Content, "10ofSpades", new Rectangle(50, 50, 142, 212));
+            aceOfDiamonds = new Card(this.Content, "AceofDiamonds", new Rectangle(200, 50, 142, 212));
+
+            base.Initialize();
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // TODO: Add your drawing code here
+            tenOfSpades.Draw(graphics.GraphicsDevice);
+            aceOfDiamonds.Draw(graphics.GraphicsDevice);
+            base.Draw(gameTime);
+        }
+    }
+}
